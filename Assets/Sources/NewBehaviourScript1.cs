42f923115c864ac8b863e3a1d6fa0a16b6d55272
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2019-01-27 14:09:46 +0900

    Initial commit.

---
+++ Assets/Sources/NewBehaviourScript1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		Screen.SetResolution(1280, 800, FullScreenMode.ExclusiveFullScreen, 60);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        
+	}
+}
